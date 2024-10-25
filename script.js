2ec41aeaf29c8d5d52a4ddb0e3656859a30b1d6e
e-korepetycje <ewam.edu@gmail.com>
2024-10-25 12:27:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+
+    hamburger.addEventListener('click', function () {
+        menu.classList.toggle('hidden'); // Przełączanie klasy ukrywającej
+    });
+});
